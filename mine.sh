18816737892fc6e42a04676023fd0f42b29ba0dc
mavx <auyongminhao@gmail.com>
2018-02-17 16:19:20 +0800

    Only kill screen with specific name

--- mine.sh
+++ mine.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-echo "Killing xmr-stak..."
-killall xmr-stak
-echo "Killing screen..."
-killall screen
+echo "Killing existing 'trtlminer' screen..."
+screen -X -S trtlminer quit
 
 echo "Running xmr-stak in screen with name 'miner'..."
 sleep 2
-screen -dmS miner bash -c 'cd ~/xmr-stak/build/bin && ./xmr-stak'
+screen -dmS trtlminer bash -c 'cd ~/xmr-stak/build/bin && ./xmr-stak'
 
 sleep 2
 ps -ef | grep miner # Show running process
