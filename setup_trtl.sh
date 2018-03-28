dd9e5fd91febb6cac7932f6b8b5dfeb9113d4db3
Au Yong Min Hao <mauyong@moneylion.com>
2018-03-28 12:35:23 +0800

    Clean install xmr-stak

--- setup_trtl.sh
+++ setup_trtl.sh
@@ -3,14 +3,16 @@ sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-
 
 # Clone & build XMR-STAK
 cd ~ # Reset directory to user HOME
+rm -rf xmr-stak/ # Remove existing xmr-stak directory
 git clone https://github.com/fireice-uk/xmr-stak.git
 mkdir xmr-stak/build
 cd xmr-stak/build
 cmake -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF ..
 make install
 
-cd bin
-cp ~/turtleminer/config.txt . # Use config file from this repo
+# Use config file from this repo
+cp ~/turtleminer/config.txt ./bin
+cp ~/turtleminer/pools.txt ./bin
 
 cd ~/turtleminer
 echo "SUCCESS!"
