eb53776108026a54018a2037a21fd1b573bd1650
Au Yong Min Hao <mauyong@moneylion.com>
2018-07-22 09:53:18 +0800

    Add xmrstak miner

---
+++ mine_xmrstak.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Killing existing 'trtlminer' screen..."
+screen -X -S trtlminer quit
+
+echo "Running xmr-stak in screen with name 'miner'..."
+sleep 2
+screen -dmS trtlminer bash -c 'cd ~/xmr-stak/build/bin && ./xmr-stak'
+
+sleep 2
+ps -ef | grep miner # Show running process
